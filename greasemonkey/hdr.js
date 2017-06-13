ea3b6ad1b09360a1315f9b440f285e10b37f8e51
theinternetftw <theinternetftw@gmail.com>
2017-06-13 00:47:32 -0400

    Rev version

--- greasemonkey/hdr.js
+++ greasemonkey/hdr.js
@@ -4,7 +4,7 @@
 // @include     https://www.netflix.com/browse
 // @include     https://www.netflix.com/browse/*
 // @include     https://www.netflix.com/title/*
-// @version     1.0.1
+// @version     1.0.2
 // @grant       GM_xmlhttpRequest
 // @grant       GM_setValue
 // @grant       GM_getValue
